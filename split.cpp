1fff8544a324447f893123598ba8a0b7145c8990
Wenwei Fu <wenweifu@usc.edu>
2025-09-13 01:47:54 -0700

    remove the libraries used in the file to prevent the loop warning

--- split.cpp
+++ split.cpp
@@ -11,8 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-#include <iostream>
-using namespace std;
+
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
